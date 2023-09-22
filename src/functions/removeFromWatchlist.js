636a47855fa5e8da67feb37cbb5007981f85d56b
Nitin Tembhare <nitintembhare340@gmail.com>
2023-09-22 13:46:00 +0530

    window reload removed

--- src/functions/removeFromWatchlist.js
+++ src/functions/removeFromWatchlist.js
@@ -13,7 +13,6 @@ export const removeFromWatchlist = (id) => {
         id.slice(0, 1).toUpperCase() + id.slice(1)
       } removed from the watchlist!`
     );
-    window.location.reload(true);
   } else {
     toast.error("Couldnt remove the coin from the watchlist!");
   }
